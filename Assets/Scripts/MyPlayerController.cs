b36237a4aa9f9a279cb75597f68bcad25dffcb02
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2024-11-23 11:45:15 +0100

    TommyProva commit con MyPlayerController.cs

--- Assets/Scripts/MyPlayerController.cs
+++ Assets/Scripts/MyPlayerController.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class MyPlayerController : MonoBehaviour
 {
+
+    [SerializeField] private float speed = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.Translate(transform.forward * speed * Time.deltaTime);
     }
 
     // Update is called once per frame

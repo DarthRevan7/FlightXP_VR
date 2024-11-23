916c10218c3fd8d7afe0ba7f2ac4f4cbc1ec8757
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2024-11-23 11:41:06 +0100

    Modifica 1 dello script MyObstacle

--- Assets/Scripts/MyObstacleScript.cs
+++ Assets/Scripts/MyObstacleScript.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class MyObstacleScript : MonoBehaviour
 {
+
+    [SerializeField] private float float1 = 34f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("34___");
     }
 
     // Update is called once per frame

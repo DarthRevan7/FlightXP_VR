7043bc823b0f7c066822f102a8481fea5781ece3
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2024-12-03 15:18:03 +0100

    Export Prefabs

---
+++ Assets/Scripts/QuestManagement/QuestUIElement.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuestUIElement", menuName = "Scriptable Objects/QuestUIElement")]
+public class QuestUIElement : ScriptableObject
+{
+    public string title, text;
+    public GameObject card;
+
+    
+}

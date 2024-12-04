a632900e2f38b7332d9080b1f902c24c2c3f6dda
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2024-12-04 11:16:06 +0100

    Primo commit Realizzazione del menù principale Interfaccia non diegetica Da provare col visore

--- Assets/Scripts/QuestManagement/QuestUIManager.cs
+++ Assets/Scripts/QuestManagement/QuestUIManager.cs
@@ -13,7 +13,7 @@ public class QuestUIManager : MonoBehaviour
 
     private void Awake() 
     {
-        questUIElements = Resources.LoadAll<QuestUIElement>("Quest\\Movement Tutorial").ToList();
+        questUIElements = Resources.LoadAll<QuestUIElement>("Quest\\Movement Tutorial\\").ToList();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created

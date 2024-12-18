ff841b70699f16348dbb8a5bba04fad6947a5fff
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2024-12-18 20:02:23 +0100

    Modifiche al mendù principale ed alla scena a terra; tentativo di abbassare l'altezza del player; corretto setting delle scene per la compilazione.

--- .utmp/Debug/526q4g44/arm64-v8a/cmake_install.cmake
+++ .utmp/Debug/526q4g44/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/GitHub/PolitoPro/FlightXP_VR_Original/Library/PramData/AndroidApplicationController/Cache/com.unity.template.vr/gradle-project/unityLibrary/src/main/cpp
+# Install script for directory: C:/GitHub/PolitoPro/FlightXP_VR_Original/Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)

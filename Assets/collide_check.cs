e8c82b8af84628de2e2df9f95b5e840dbf2dd1c4
Andrea Fasano <andrea17.fasano@gmail.com>
2025-02-09 11:50:16 +0100

    added info lights

--- Assets/collide_check.cs
+++ Assets/collide_check.cs
@@ -5,12 +5,16 @@ public class TerrainCollisionDetector : MonoBehaviour
     public GameObject plane;
     public float checkDistance = 100f; // Adjust based on object size
     public float collisionDistance = 1.0f;
+    public float dangerDistance = 50.0f;
     public Vector3[] directions = {
         Vector3.down, Vector3.up, Vector3.left, Vector3.right, Vector3.forward, Vector3.back
     };
 
+    public bool danger = false;
+
     void Update()
     {
+        danger = false;
         foreach (Vector3 direction in directions)
         {
             if (Physics.Raycast(transform.position, direction, out RaycastHit hit, checkDistance))
@@ -19,7 +23,9 @@ public class TerrainCollisionDetector : MonoBehaviour
                 if (hit.collider.GetComponent<Terrain>())
                 {
                     //Debug.Log($"Collided with terrain! {hit.distance} in direction {direction}");
-                    if(hit.distance < collisionDistance)
+                    if (hit.distance < dangerDistance) danger = true;
+
+                    if (hit.distance < collisionDistance)
                     {
                         Debug.Log("Collision!!");
                     }
